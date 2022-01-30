39b1c15eb23e1532327aa186f363330d6f075604
sakai135 <sakai135@users.noreply.github.com>
2022-01-30 02:37:41 -0500

    use pipeline

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.IO;
+using System.IO.Pipelines;
 using System.IO.Pipes;
 using System.Threading.Tasks;
 using PipeOptions = System.IO.Pipes.PipeOptions;
 
 const string serverName = ".";
-const int bufferSize = 8192;
+const int minimumBufferSize = 8192;
 
 if (!Console.IsInputRedirected || !Console.IsOutputRedirected)
 {
@@ -30,10 +31,15 @@ using
     ((NamedPipeClientStream)namedPipe).Connect();
     Log("Connected");
 
+    var pipeFromInput = new Pipe();
+    var pipeToOutput = new Pipe();
+
     await Task.WhenAll
     (
-        Task.Run(() => Copy(input, namedPipe)),
-        Task.Run(() => Copy(namedPipe, output))
+        Read(pipeFromInput.Writer, input),
+        Write(pipeFromInput.Reader, namedPipe),
+        Read(pipeToOutput.Writer, namedPipe),
+        Write(pipeToOutput.Reader, output)
     );
 }
 
@@ -42,10 +48,63 @@ void Log(string msg)
     Console.Error.WriteLine(msg);
 }
 
-void Copy(Stream input, Stream output)
+void LogException(Exception ex)
+{
+    Console.Error.WriteLine(ex.Message);
+    Console.Error.WriteLine(ex.StackTrace);
+}
+
+async Task Read(PipeWriter writer, Stream input)
 {
     while (true)
     {
-        input.CopyTo(output, bufferSize);
+        var memory = writer.GetMemory(minimumBufferSize);
+        try
+        {
+            var bytesRead = await input.ReadAsync(memory);
+            // Log($"read {bytesRead}");
+            if (bytesRead == 0)
+            {
+                break;
+            }
+            writer.Advance(bytesRead);
+        }
+        catch (Exception ex)
+        {
+            LogException(ex);
+            break;
+        }
+        var result = await writer.FlushAsync();
+        if (result.IsCompleted)
+        {
+            break;
+        }
     }
+    writer.Complete();
+}
+
+async Task Write(PipeReader reader, Stream output)
+{
+    while (true)
+    {
+        var result = await reader.ReadAsync();
+        var seq = result.Buffer;
+        if (seq.IsSingleSegment)
+        {
+            await output.WriteAsync(seq.First);
+        }
+        else
+        {
+            foreach (var mem in seq)
+            {
+                await output.WriteAsync(mem);
+            }
+        }
+        reader.AdvanceTo(seq.End);
+        if (result.IsCompleted)
+        {
+            break;
+        }
+    }
+    reader.Complete();
 }
