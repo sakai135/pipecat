9a33376aa4bfa527ed540ccbd0d39656496a5771
sakai135 <sakai135@users.noreply.github.com>
2022-02-01 21:50:36 -0500

    simplify pipeline

--- Program.cs
+++ Program.cs
@@ -22,23 +22,18 @@ var pipeName = args[0];
 Log($"Connecting to \\\\{serverName}\\pipe\\{pipeName} ...");
 using
 (
-    Stream namedPipe = new NamedPipeClientStream(serverName, pipeName, PipeDirection.InOut, NamedPipeOptions.Asynchronous),
-    input = Console.OpenStandardInput(),
-    output = Console.OpenStandardOutput()
+    Stream npipe = new NamedPipeClientStream(serverName, pipeName, PipeDirection.InOut, NamedPipeOptions.Asynchronous),
+    stdin = Console.OpenStandardInput(),
+    stdout = Console.OpenStandardOutput()
 )
 {
-    ((NamedPipeClientStream)namedPipe).Connect();
+    ((NamedPipeClientStream)npipe).Connect();
     Log("Connected");
 
-    var pipeFromInput = new Pipe();
-    var pipeToOutput = new Pipe();
-
-    Parallel.Invoke
-    (
-        () => Read(pipeFromInput.Writer, input).Wait(),
-        () => Write(pipeFromInput.Reader, namedPipe).Wait(),
-        () => Read(pipeToOutput.Writer, namedPipe).Wait(),
-        () => Write(pipeToOutput.Reader, output).Wait()
+    await Task.WhenAll
+    (     
+        Connect(stdin, npipe),
+        Connect(npipe, stdout)
     );
 }
 
@@ -47,50 +42,11 @@ void Log(string msg)
     Console.Error.WriteLine(msg);
 }
 
-async Task Read(PipeWriter writer, Stream input)
+async Task Connect(Stream input, Stream output)
 {
+    var reader = PipeReader.Create(input);
     while (true)
     {
-        var task = InnerRead(writer, input);
-        var result = task.IsCompletedSuccessfully ? task.Result : await task;
-        if (result.IsCompleted)
-        {
-            break;
-        }
+        await reader.CopyToAsync(output);
     }
-    writer.Complete();
-}
-
-ValueTask<FlushResult> InnerRead(PipeWriter writer, Stream input)
-{
-    var span = writer.GetSpan();
-    var bytesRead = input.Read(span);
-    writer.Advance(bytesRead);
-    return writer.FlushAsync();
-}
-
-async Task Write(PipeReader reader, Stream output)
-{
-    while (true)
-    {
-        var result = await reader.ReadAsync();
-        var seq = result.Buffer;
-        if (seq.IsSingleSegment)
-        {
-            output.Write(seq.FirstSpan);
-        }
-        else
-        {
-            foreach (var mem in seq)
-            {
-                output.Write(mem.Span);
-            }
-        }
-        reader.AdvanceTo(seq.End);
-        if (result.IsCompleted)
-        {
-            break;
-        }
-    }
-    reader.Complete();
 }
